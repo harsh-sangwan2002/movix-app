8df7c26a295498073d634b5f69022c1421ef666b
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-12-02 17:22:43 +0530

    added

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
 
-    baseURL:"http://localhost:8800/api/",
+    baseURL:"https://movix-app-giym.onrender.com/api/",
     withCredentials:true
 })
 
